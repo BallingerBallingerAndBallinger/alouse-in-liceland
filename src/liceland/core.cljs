7e38bca30315bbba58dac4d109e022bd95d8edc2
Andrew Ballinger <andrewballinger@stratisopt.com>
2017-04-21 22:57:35 -0700

    Draw images

--- src/liceland/core.cljs
+++ src/liceland/core.cljs
@@ -9,13 +9,32 @@
 (defonce width (.getAttribute app "width"))
 (defonce height (.getAttribute app "width"))
 
+(defn set-cursor [cursor]
+  (case cursor
+    :left (.setAttribute app "class" "left-cursor")
+    (.setAttribute app "class" "")))
+
+(defonce watch-mouse-move
+  (aset app "onmousemove"
+                     (fn [e]
+                       (this-as element
+                         (if (< (- (.-pageX e) (.-offsetLeft element)) (* width 0.2))
+                           (set-cursor :left)
+                           (set-cursor :none))))))
+
 (def raw-image-data (.getImageData context 0 0 width height))
 (defn draw [draw-fn]
   (do (draw-fn (-> raw-image-data .-data ta/uint32-view))
       (.putImageData context raw-image-data 0 0)))
+(defn draw-image [image x y]
+  (let [img (js/Image.)]
+    (do
+      (aset img "onload" #(.drawImage context img x y))
+      (aset img "src" "/images/hairs-low.png"))))
 
 ;; Make her red to demonstrate functioning
 (draw #(.fill % 0xff0000ff))
+(draw-image :hairs 0 0)
 
 (defn on-js-reload []
   (.log js/console "Reload success!"))

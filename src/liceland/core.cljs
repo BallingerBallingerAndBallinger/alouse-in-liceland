a2b352a0d5be7f11e20231e1ffd67ab82f4401eb
Andrew Ballinger <andrewballinger@stratisopt.com>
2017-04-22 00:30:10 -0700

    Proper rendering with an image loader and a text-box, no deep structure

--- src/liceland/core.cljs
+++ src/liceland/core.cljs
@@ -1,5 +1,6 @@
 (ns liceland.core
-  (:require [thi.ng.typedarrays.core :as ta]))
+  (:require
+   [thi.ng.typedarrays.core :as ta]))
 
 (enable-console-print!)
 (println "This text is printed from src/liceland/core.cljs. Go ahead and edit it and see reloading in action.")
@@ -7,7 +8,24 @@
 (defonce app (.getElementById js/document "app"))
 (defonce context (.getContext app "2d"))
 (defonce width (.getAttribute app "width"))
-(defonce height (.getAttribute app "width"))
+(defonce height (.getAttribute app "height"))
+(defonce image-cache (atom {}))
+(defonce images {:hairs "/images/hairs-low.png"})
+
+(declare on-images-loaded)
+
+(defn cache-image [symbol image]
+  (do (swap! image-cache assoc symbol image)
+      (if (= (count @image-cache) (count images)) (on-images-loaded))))
+
+(defn load-image [symbol url]
+  (let [img (js/Image.)]
+    (do
+      (.log js/console (str "Loading image for " (name symbol)))
+      (aset img "onload" #(cache-image symbol img))
+      (aset img "src" url))))
+
+(defonce load-images (doall (map load-image (keys images) (vals images))))
 
 (defn set-cursor [cursor]
   (case cursor
@@ -22,19 +40,22 @@
                            (set-cursor :left)
                            (set-cursor :none))))))
 
-(def raw-image-data (.getImageData context 0 0 width height))
 (defn draw [draw-fn]
-  (do (draw-fn (-> raw-image-data .-data ta/uint32-view))
-      (.putImageData context raw-image-data 0 0)))
+  (let [raw-image-data (.getImageData context 0 0 width height)]
+       (do (draw-fn (-> raw-image-data .-data ta/uint32-view))
+           (.putImageData context raw-image-data 0 0))))
+
 (defn draw-image [image x y]
-  (let [img (js/Image.)]
-    (do
-      (aset img "onload" #(.drawImage context img x y))
-      (aset img "src" "/images/hairs-low.png"))))
+  (.drawImage context (image @image-cache) x y))
 
 ;; Make her red to demonstrate functioning
-(draw #(.fill % 0xff0000ff))
-(draw-image :hairs 0 0)
+(defn on-images-loaded []
+          (draw #(.fill % 0xfffff0ff))
+          (draw-image :hairs 0 0)
+          (draw (fn [pixels]
+                  (doall
+                   (map #(aset pixels % 0xff000000)
+                        (range (* width (* 0.8 height)) (* width height)))))))
 
 (defn on-js-reload []
   (.log js/console "Reload success!"))

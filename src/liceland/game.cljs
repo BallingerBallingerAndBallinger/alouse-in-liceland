80cedb718804c0c385f475e79aa4c644ed922f08
Andrew Ballinger <andrewballinger@stratisopt.com>
2017-04-23 00:25:29 -0700

    Add some documentation before conking out

--- src/liceland/game.cljs
+++ src/liceland/game.cljs
@@ -3,7 +3,16 @@
 (defonce app (.getElementById js/document "app"))
 (defonce width (.getAttribute app "width"))
 (defonce height (.getAttribute app "height"))
+(declare forest)
 (declare mosquito-dialog)
+
+;; Sprites permit the following properties
+;; sound: URL of a sound file to play when the sprite is visible (looping)
+;; positionX: The x position of the sprite
+;; positionY: The y position of the sprite
+;; image:     URL of an image file for the sprite
+;; click:     A symbol pointing to a scene to jump to when the sprite is clicked
+;; scale:     A scaling factor
 
 (defn clickable [sprite target]
   (cljs.core/merge sprite {:click target}))
@@ -14,6 +23,7 @@
    :positionY (* 0.34 height)
    :image "/images/mosquito-flit1.png"})
 
+;; Demonstrating one way of building things up from simpler pieces.
 (def larger-mosquito
   (cljs.core/merge mosquito
                    {:positionX (* 0.4 width)
@@ -26,6 +36,16 @@
                     :positionY (* -0.2 width)
                     :scale 16}))
 
+;; Scenes permit the following properties
+;; background:  URL of a background for the scene
+;; description: Text that appears beneath a scene
+;; music:       Music to play while you're in the scene
+;; right:       A symbol pointing to a scene to jump to when right is clicked
+;; left:        A symbol pointing to a scene to jump to when left is clicked
+;; forward:     A symbol pointing to a scene to jump to when forward is clicked
+;; sprites:     An array of sprites present in the scene
+;; update:      A function to update the state when a scene is entered.... AND FOR THE LOVE OF GOD
+
 ;; Be careful with the state...
 ;; It should be used extremely sparingly
 ;; e.g. It's easy to subtly break live-reloading by getting something stuck in your state.
@@ -33,39 +53,44 @@
 ;;      have the same state that you do...
 ;; e.g. Asset-preloading depends on sprites, backgrounds, music, and sounds to be reachable with an empty state.
 ;; However, with all of those warnings, having access to it give you great power, and with great power comes...! (Mayhem!)
+;; You will find that things are happy and easy and everything is great and there are rainbows and unicorns
+;; unless you ABUSE THE STATE THEN DEVIL HIMSELF WILL CRAWL SCREAMING OUT OF YOUR BUTTHOLE.
 
 (defn scenes [state]
   (merge (mosquito-dialog state)
-         {:head-west {:background "/images/hairs-low.png"
-                      :description "Nothing but trees"
-                      :music "/audio/liceland.mp3"
-                      :right :head
-                      :left :head-east }
-          :head {:background "/images/hairs-low.png"
-                 :description "A vast forest stretches as far as the eye can see"
-                 :music "/audio/liceland.mp3"
-                 :left :head-west
-                 :right :head-east }
-          :heading-on {:background "/images/hairs-low.png"
-                       :forward :heading-on-2
-                       :sprites [ (clickable larger-mosquito :heading-on-2) ]
-                       :music "/audio/liceland.mp3"
-                       :description "It just keeps going"}
-          :heading-on-2 {:background "/images/hairs-low.png"
-                         :forward :heading-on-3
-                         :music "/audio/liceland.mp3"
-                         :sprites [ (if (not (:talked-to-mosq state))
-                                      (clickable largest-mosquito :lookin-at-me)
-                                      (clickable largest-mosquito :not-lookin-at-me))]}
-          :heading-on-3 {:background "/images/hairs-low.png"
-                         :description "You've lost your way in the immensity"
-                         :forward :head-east }
-          :head-east {:background "/images/hairs-low.png"
-                      :forward :heading-on
-                      :music "/audio/liceland.mp3"
-                      :sprites [ (clickable mosquito :heading-on) ]
-                      :right :head-west
-                      :left :head}}))
+         (forest state)))
+
+(defn forest [state]
+  {:head-west {:background "/images/hairs-low.png"
+               :description "Nothing but trees"
+               :music "/audio/liceland.mp3"
+               :right :head
+               :left :head-east }
+   :head {:background "/images/hairs-low.png"
+          :description "A vast forest stretches as far as the eye can see"
+          :music "/audio/liceland.mp3"
+          :left :head-west
+          :right :head-east }
+   :heading-on {:background "/images/hairs-low.png"
+                :forward :heading-on-2
+                :sprites [ (clickable larger-mosquito :heading-on-2) ]
+                :music "/audio/liceland.mp3"
+                :description "It just keeps going"}
+   :heading-on-2 {:background "/images/hairs-low.png"
+                  :forward :heading-on-3
+                  :music "/audio/liceland.mp3"
+                  :sprites [ (if (not (:talked-to-mosq state))
+                               (clickable largest-mosquito :lookin-at-me)
+                               (clickable largest-mosquito :not-lookin-at-me))]}
+   :heading-on-3 {:background "/images/hairs-low.png"
+                  :description "You've lost your way in the immensity"
+                  :forward :head-east }
+   :head-east {:background "/images/hairs-low.png"
+               :forward :heading-on
+               :music "/audio/liceland.mp3"
+               :sprites [ (clickable mosquito :heading-on) ]
+               :right :head-west
+               :left :head}})
 
 (defn mosquito-dialog [state]
   ;; Demonstrating how a base scene can be extended...  Imagine the possibilities.
